06e7efeef870b6836357a84de88f8c37ba5d5321
ramseey <ber14271@spengergasse.at>
2013-09-26 11:50:55 +0200

    Update Hallo.java
    lol

--- 4bhifs/src/Hallo.java
+++ 4bhifs/src/Hallo.java
@@ -3,7 +3,7 @@ public class Hallo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hallo git2222");
-		System.out.println("hallo 44444 ");
+		System.out.println("hallo 444sadfasfasdfasdfasdfafd44 ");
 
 	}
 
