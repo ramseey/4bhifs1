c59909e422c8c3451291c4b652e0374c6e8f04b5
htlspenger2013 <htl-spenger-2013@gmx.at>
2013-09-26 11:51:45 +0200

    konflikt lokal

--- 4bhifs/src/Hallo.java
+++ 4bhifs/src/Hallo.java
@@ -4,7 +4,7 @@ public class Hallo {
 		// TODO Auto-generated method stub
 		System.out.println("hallo git2222");
 		System.out.println("hallo ");
-
+		System.out.println("hallo zum dritten");
 	}
 
 }
