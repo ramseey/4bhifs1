66c6ed537bb4cc74b00d853917130aa804fe1bb1
htlspenger2013 <htl-spenger-2013@gmx.at>
2013-09-26 11:49:25 +0200

    Update Hallo.java

--- 4bhifs/src/Hallo.java
+++ 4bhifs/src/Hallo.java
@@ -3,7 +3,7 @@ public class Hallo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hallo git2222");
-		System.out.println("hallo ");
+		System.out.println("hallo 44444 ");
 
 	}
 
